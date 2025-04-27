f294e5314cac12a3ad4a6aa9d731311751225599
Maria Root <mariaroot1@gmail.com>
2025-04-27 17:21:48 +0100

    Tidied up remainder of code, added markdown

--- BDTT_Task_1_Analysis_of_Clinical_Trial_Data.sql
+++ BDTT_Task_1_Analysis_of_Clinical_Trial_Data.sql
@@ -4,26 +4,28 @@
 -- MAGIC # BDTT Task 1: Analysis of Clinical Trial Data Using Spark SQL
 -- MAGIC
 -- MAGIC ## Introduction
+-- MAGIC For this task, I performed some analysis of data from clinical trials registered in the USA. First I imported and pre-processed the data, then I used SQL to query the data to answer 4 specific questions.
 -- MAGIC
--- MAGIC _In this section you should briefly introduce the task and briefly summarise your understanding of what you need to do. It can be relatively brief and is just intended to frame and introduce the rest of the notebook. You should consider that you are writing this for a technical audience._
--- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %md
 -- MAGIC ## Data Import and Preprocessing
 -- MAGIC
--- MAGIC _In this section you should briefly explain the data import and preprocessing steps you have gone through to read the data into a DataFrame and clean it prior to creating the necessary tables for your SQL analysis in the following code cells. The aim is to provide explanation / justification for the techniques you have used. If there are any notable features of the data which have required handling in the pre-processing stage you should mention them here._
--- MAGIC
+-- MAGIC In this section, I read the data from a csv file into a dataframe. I then ensured that the data in every column was of the correct datatype (changing the datatype if not). I also counted the number of null values in each column and checked the format of columnns containing lists. All these features will be important for the questions I have to answer subsequently. 
 
 -- COMMAND ----------
 
 -- MAGIC %python
 -- MAGIC
--- MAGIC # Checking that the Clinicaltrial_16012025.csv file was uploaded successfully to the DBFS 
+-- MAGIC # Check that the Clinicaltrial_16012025.csv file was uploaded successfully to the DBFS 
 -- MAGIC
 -- MAGIC dbutils.fs.ls("/FileStore/tables")
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC # Read Clinicaltrial_16012025.csv file into dataframe (df1) without splitting lines in columns that contain lists (eg Conditions column). Show first 5 rows and count how many rows in total 
+-- MAGIC # Read Clinicaltrial_16012025.csv file into dataframe (df1) without splitting lines in columns that contain lists (eg Conditions column). 
 -- MAGIC
 -- MAGIC df1 = spark.read.csv(
 -- MAGIC "/FileStore/tables/Clinicaltrial_16012025.csv",
@@ -31,6 +33,8 @@
 -- MAGIC     inferSchema=True, # Infer data types
 -- MAGIC     multiLine=True # Enable multiline support
 -- MAGIC )
+-- MAGIC
+-- MAGIC # Show first 5 rows and count how many rows in total 
 -- MAGIC
 -- MAGIC df1.show(5)
 -- MAGIC Number_of_rows = df1.count()
@@ -45,7 +49,7 @@
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC # Converting Enrollment column from default string datatype to integer datatype 
+-- MAGIC # Convert Enrollment column from default string datatype to integer datatype 
 -- MAGIC
 -- MAGIC df1 = df1.withColumn("Enrollment", df1.Enrollment.cast("int"))
 -- MAGIC
@@ -53,7 +57,7 @@
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC # Inspecting contents of columns that contain dates to determine how to cast them from the default string datatype to date datatype
+-- MAGIC # Inspect contents of columns that contain dates to determine how to cast them from the default string datatype to date datatype
 -- MAGIC
 -- MAGIC date_columns = df1[['Start Date', 'Completion Date']]
 -- MAGIC date_columns.show(10, truncate=False)
@@ -69,17 +73,21 @@
 -- MAGIC from pyspark.sql.functions import col
 -- MAGIC
 -- MAGIC # Define regular expressions
+-- MAGIC
 -- MAGIC regex_yyyy_mm_dd = r'^\d{4}-\d{2}-\d{2}$'  # Matches YYYY-MM-DD
 -- MAGIC regex_yyyy_mm = r'^\d{4}-\d{2}$'  # Matches YYYY-MM
 -- MAGIC
 -- MAGIC # Count Start Date rows that match each pattern
+-- MAGIC
 -- MAGIC SD_count_yyyy_mm_dd = df1.filter(col("Start Date").rlike(regex_yyyy_mm_dd)).count()
 -- MAGIC SD_count_yyyy_mm = df1.filter(col("Start Date").rlike(regex_yyyy_mm)).count()
 -- MAGIC
 -- MAGIC # Total count of Start Date rows matching either pattern 
+-- MAGIC
 -- MAGIC SD_total_count = SD_count_yyyy_mm_dd + SD_count_yyyy_mm
 -- MAGIC
 -- MAGIC # Calculate percentage and round to the nearest whole number
+-- MAGIC
 -- MAGIC SD_percentage = round((SD_total_count / Number_of_rows) * 100)
 -- MAGIC
 -- MAGIC print("Number of Start Date entries conforming to YYYY-MM-DD or YYYY-MM format:", SD_total_count)
@@ -91,13 +99,16 @@
 -- MAGIC %python
 -- MAGIC
 -- MAGIC # Count Completion Date rows that match each pattern
+-- MAGIC
 -- MAGIC CD_count_yyyy_mm_dd = df1.filter(col("Completion Date").rlike(regex_yyyy_mm_dd)).count()
 -- MAGIC CD_count_yyyy_mm = df1.filter(col("Completion Date").rlike(regex_yyyy_mm)).count()
 -- MAGIC
 -- MAGIC # Total count of Completion Date rows matching either pattern 
+-- MAGIC
 -- MAGIC CD_total_count = CD_count_yyyy_mm_dd + CD_count_yyyy_mm
 -- MAGIC
 -- MAGIC # Calculate percentage and round to the nearest whole number
+-- MAGIC
 -- MAGIC CD_percentage = round((CD_total_count / Number_of_rows) * 100)
 -- MAGIC
 -- MAGIC print("Number of Completion Date entries conforming to YYYY-MM-DD or YYYY-MM format:", CD_total_count)
@@ -117,6 +128,7 @@
 -- MAGIC from pyspark.sql.functions import to_date, when, length, col, concat, lit
 -- MAGIC
 -- MAGIC # Convert Start date and Completion date columns from string format to date format
+-- MAGIC
 -- MAGIC df1 = df1.withColumn(
 -- MAGIC     "Start Date",
 -- MAGIC     when(length(col("Start Date")) == 7, to_date(concat(col("Start Date"), lit("-01")), "yyyy-MM-dd"))  # Handle YYYY-MM
@@ -130,6 +142,7 @@
 -- MAGIC )
 -- MAGIC
 -- MAGIC # Check datatypes and contents of start date and completion date columns to check that the datatype conversion above worked 
+-- MAGIC
 -- MAGIC date_columns = df1[['Start Date', 'Completion Date']]
 -- MAGIC date_columns.show(10, truncate=False)
 
@@ -141,7 +154,7 @@
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC # Counting how many null values in each column
+-- MAGIC # Count how many null values in each column
 -- MAGIC
 -- MAGIC from pyspark.sql.functions import col, sum
 -- MAGIC df1.select([sum(col(c).isNull().cast("int")).alias(c) for c in df1.columns]).show()
@@ -150,12 +163,13 @@
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC Now I'll inspect the format of the lists in 2 of the columns of df1. 
+-- MAGIC Now I'll inspect the format of the lists in two of the columns of df1. 
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC # Examining 2 columns that contain lists to see how the lists are formatted
+-- MAGIC # Examine two columns that contain lists to see how the lists are formatted
+-- MAGIC
 -- MAGIC selected_columns = df1[['Conditions', 'Interventions']]
 -- MAGIC selected_columns.show(5, truncate=False)
 
@@ -171,15 +185,14 @@
 -- MAGIC
 -- MAGIC **List all the clinical trial types (as contained in the Type column of the data) along with their frequency, sorting the results from most to least frequent**
 -- MAGIC
--- MAGIC You should start each section with a markdown cell providing a brief explanation of the code. It should be aimed at providing enough information on the functions and methods you have used that a person with a similar tehcnical background can understand **why** you have used them.
--- MAGIC
--- MAGIC You can also include formatted code snippets like this ```print("Hello World")``` in a markdown cell. To do this, simply wrap in these characters at the beginning: "\```" and these characters at the end: "\```"_
+-- MAGIC To answer this question, I used SQL Views. This allows me to show only certain parts of the data without making any lasting changes to the underlying dataframe 
 
 -- COMMAND ----------
 
 -- MAGIC %python
 -- MAGIC
 -- MAGIC # Create temporary view of df1
+-- MAGIC
 -- MAGIC df1.createOrReplaceTempView("ct1")
 
 -- COMMAND ----------
@@ -191,6 +204,7 @@ SELECT * FROM ct1 LIMIT 5
 -- COMMAND ----------
 
 -- Create smaller view of just the 'Study Type' column 
+
 CREATE OR REPLACE TEMP VIEW types AS SELECT `Study Type` FROM ct1
 
 -- COMMAND ----------
@@ -210,7 +224,7 @@ ORDER BY COUNT(*) DESC;
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC # Question 2
+-- MAGIC ## Question 2
 -- MAGIC
 -- MAGIC **List the top 10 conditions along with their frequency**
 -- MAGIC
@@ -225,21 +239,25 @@ CREATE OR REPLACE TEMP VIEW split_conditions AS SELECT split(Conditions , '\\|')
 -- COMMAND ----------
 
 -- Check the code above worked 
+
 SELECT * FROM split_conditions LIMIT 10
 
 -- COMMAND ----------
 
 -- Create another temporary view, exploding out the arrays in each row of the consitions column above so that each item in the array (that is, each condition) is on a separate row
+
 CREATE OR REPLACE TEMP VIEW exploded_conditions AS SELECT explode(conditions) FROM split_conditions
 
 -- COMMAND ----------
 
 -- Check the code above worked 
+
 SELECT * FROM exploded_conditions LIMIT 10
 
 -- COMMAND ----------
 
 -- Count the frequenct of each condition, order the conditions from most to least frequent, show the top 10 most frequent conditions 
+
 SELECT `col`, COUNT(*) as frequency
 FROM exploded_conditions
 GROUP BY `col`
@@ -253,9 +271,11 @@ ORDER BY COUNT(*) DESC LIMIT 10;
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC # Question 3
+-- MAGIC ## Question 3
 -- MAGIC
 -- MAGIC **For studies with an end date, calculate the mean clinical trial length in months.**
+-- MAGIC
+-- MAGIC As for question 1, I will use SQL views to answer this question without altering the underlying dataframe 
 
 -- COMMAND ----------
 
@@ -272,6 +292,7 @@ SELECT * FROM ct2 LIMIT 20
 -- COMMAND ----------
 
 -- Add column that calculates Duration of each trial in days by subtracting the start date from the end date. Use ABS to return only positive integers (that is, exclude any rows where the start date is after the end date)
+
 CREATE OR REPLACE TEMP VIEW ct3 AS
 SELECT *, 
        ABS(DATEDIFF(MONTH, `Completion Date`, `Start Date`)) AS Duration
@@ -281,6 +302,7 @@ FROM ct2;
 -- COMMAND ----------
 
 -- Check above code worked 
+
 SELECT * FROM ct3 LIMIT 10
 
 -- COMMAND ----------
@@ -297,6 +319,7 @@ SELECT ROUND(AVG(Duration), 0) AS Average_Duration FROM ct3
 -- COMMAND ----------
 
 -- Count frequencies of trial durations and sort from longest duration to shortest duration 
+
 SELECT `Duration`, COUNT(*) as frequency
 FROM ct3
 GROUP BY `Duration`
@@ -310,7 +333,7 @@ ORDER BY `Duration` DESC;
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC # Question 4
+-- MAGIC ## Question 4
 -- MAGIC
 -- MAGIC **From the studies with a non-null completion date and a status of ‘Completed’ in the Study Status, calculate how many of these related to Diabetes each year. Display the trend over time in an appropriate visualisation.** 
 -- MAGIC
@@ -327,23 +350,26 @@ CREATE OR REPLACE TEMP VIEW ct4 AS SELECT `Conditions`, `Study Status`, `Start D
 -- COMMAND ----------
 
 -- Check the code above worked 
+
 SELECT * FROM ct4 LIMIT 10
 
 -- COMMAND ----------
 
 
 -- From the above temoprary view, show only the rows that contain the word 'diabetes' (case-insenstive) in the 'Conditions' column 
+
 CREATE OR REPLACE TEMP VIEW ct5 AS SELECT * FROM ct4 WHERE Conditions ILIKE '%diabetes%';
 
 
 -- COMMAND ----------
 
+-- Check the above code worked
+
 SELECT * FROM ct5  LIMIT 10;
 
 -- COMMAND ----------
 
--- count how many EACH YEAR 
--- visualise 
+-- Add column called 'Year' which shows only the completion year (not the month or date)
 
 CREATE OR REPLACE TEMP VIEW ct6 AS
 SELECT *, 
@@ -354,6 +380,9 @@ SELECT * FROM ct6  LIMIT 10;
 
 -- COMMAND ----------
 
+-- Count how many diabetes studies completed in EACH YEAR 
+-- Visualise with scatter plot 
+
 SELECT COUNT(Conditions), Year
 FROM ct6
 GROUP BY Year
@@ -362,10 +391,12 @@ ORDER BY Year DESC;
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC Use a markdown cell at the end of the question to briefly explain the output / relate this back to the question and to comment or explain anything significant about this finding. Follow this structure (or a similar structure) for all questions / sections of your submission.
--- MAGIC
--- MAGIC If you want to add additional markdown cells to provide further explanation throughout your notebook, add these as required.
+-- MAGIC As the scatter plot above shows, there were very few studies on diabetes completed between 1989 and 2000. From 2000 onwards, the number of completed diabetes studies increased steadily up to a peak of 755 in 2019. Between 2019 and 2024, the number of studies seems to be decreasing. This could be because there are fewer diabetes studies being conducted, or because diabetes studies started since 2019 have not yet completed, or a combination of these two factors. 
+
+-- COMMAND ----------
+
+-- MAGIC %md
 -- MAGIC
 -- MAGIC ## Conclusion
 -- MAGIC
--- MAGIC Finish with a brief conclusion to wrap up your submission and any key findings.
+-- MAGIC In conclusion, I used python to import and pre-process a large dataset on clinical trials. I then used SQL to query this dataset to answer 4 specific questions to gain insight into this dataset. 
